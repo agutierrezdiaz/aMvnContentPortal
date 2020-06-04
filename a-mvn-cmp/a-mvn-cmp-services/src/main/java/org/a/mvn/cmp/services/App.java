0aeb0f6f1012b4c9a854d72b65f2e3834bf3a89b
agutierrezdiaz <agutierrezdiaz90@gmail.com>
2020-06-04 20:21:11 +0200

    Initial bundle

---
+++ a-mvn-cmp/a-mvn-cmp-services/src/main/java/org/a/mvn/cmp/services/App.java
@@ -0,0 +1,13 @@
+package org.a.mvn.cmp.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
